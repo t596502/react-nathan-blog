9964642bb6d50765a0e2b10a0116433b9d54308e
Nathan Tai <>
2019-05-15 14:48:07 +0800

    新增header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './pages/home/home'
+function App() {
+
+  return (
+    <div className="App">
+        <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Headernav from '../../components/header/index.js'
+
+class Header extends Component{
+    render() {
+        return(
+            <div>
+                <Headernav />
+            </div>
+        )
+    }
+}
+
+export default Header

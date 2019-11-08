a4eb3417a88fd512f318d049dffc716eef8a30fc
Nathan Tai <nathantai@outlook.com>
2019-11-08 18:52:27 +0800

    1.新增pageNotFound404组件

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import React,{ Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from '@/store'
 import { Spin } from 'antd';
 
-const Index = lazy(()=> import('@/pages/index/index'));
-
+const Index = lazy(() => import('@/pages/index/index'));
 const Archives = lazy(() => import('@/pages/archives/archives'))
 const Home = lazy(() => import('@/pages/home/home'))
 const Article = lazy(() => import('@/pages/article/article'))
 const About = lazy(() => import('@/pages/about/about'))
-
+const PageNotFound = lazy(() => import('@/components/pageNotFound'))
 
 function loading() {
-    return(
-        <Spin style={{position:'absolute',top:'50%',left:'50%',transform:'translate(-50%,-50%)'}} tip="Loading..." size="large" />
-    )
+  return (
+    <Spin style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)' }} tip="Loading..." size="large" />
+  )
 }
 
 function App() {
 
   return (
-      <Provider store={store}>
-          <HashRouter forceRefresh={true}>
-              <Suspense fallback={loading()}>
-              <Switch>
-                  {/*<Route path='/' component={Index} />*/}
-                  <Route path='/' render={()=>(
-                      <Index>
-                          <Switch>
-                              <Route exact path='/' component={Home}/>
-                              <Route path='/article/:id' component={Article}/>
-                              <Route path='/about' component={About}/>
-                              <Route path='/archives' component={Archives}/>
-                          </Switch>
-                      </Index>
-                  )} />
-              </Switch>
-              </Suspense>
-          </HashRouter> 
-      </Provider>
+    <Provider store={store}>
+      <HashRouter forceRefresh={true}>
+        <Suspense fallback={loading()}>
+          <Switch>
+            {/*<Route path='/' component={Index} />*/}
+            <Route path='' exact render={() => (
+              <Index>
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route path='/article/:id' component={Article} />
+                  <Route path='/about' component={About} />
+                  <Route path='/archives' component={Archives} />
+                  <Route path="*" component={PageNotFound} />
+                </Switch>
+              </Index>
+            )} />
+            
+          </Switch>
+        </Suspense>
+      </HashRouter>
+    </Provider>
 
   );
 }
